c51e25f3d26ac20a050693c8e6a1990f0af48743
IvanKustarev <ivan.kustarev@gmail.com>
2021-10-06 00:05:22 +0300

    connect to BD

---
+++ src/main/java/com/Ivan/DBWork/Connector.java
@@ -0,0 +1,30 @@
+package com.Ivan.DBWork;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+    private Connection connection;
+
+    public Connector() throws SQLException {
+        connect();
+    }
+    private Connection connect() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/studs", "s309681", "yvr557");
+        try {
+            connection.setAutoCommit(false);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+}
--- src/main/java/com/Ivan/DBWork/DBWorker.java
+++ src/main/java/com/Ivan/DBWork/DBWorker.java
@@ -2,9 +2,21 @@ package com.Ivan.DBWork;
 
 import com.Ivan.Values;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBWorker implements DBWorking {
+
+    Connection connection;
+
+    //здесь нужно что-то сделать с пробросом ошибки
+    public DBWorker() throws SQLException {
+        Connector connector = new Connector();
+        Connection connection = connector.getConnection();
+    }
+
     @Override
     public void addValues(Values values) {
 
